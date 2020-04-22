a2e238c64a5487112ab431492595b253cd6523d5
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2020-04-22 14:14:52 +0700

    build

--- src/main/java/com/training/config/JpaConfig.java
+++ src/main/java/com/training/config/JpaConfig.java
@@ -61,8 +61,8 @@ public class JpaConfig {
     Properties additionalProperties() {
         Properties properties = new Properties();
 //		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
-//		properties.setProperty("hibernate.hbm2ddl.auto", "none");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		properties.setProperty("hibernate.hbm2ddl.auto", "none");
         properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
         return properties;
     }
