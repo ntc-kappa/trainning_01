146451dabc5b8ab8a08444166d03d87febcd42e2
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2020-04-22 14:07:43 +0700

    build

---
+++ src/main/java/com/training/entity/User.java
@@ -0,0 +1,18 @@
+package com.training.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
