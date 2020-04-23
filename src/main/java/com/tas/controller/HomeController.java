55ef1ab5186592fdc9ae18830f9be8e4b28f824d
Do Thanh Quang <quangdtptit@gmail.com>
2020-04-23 21:25:43 +0700

    Thanh Qaung sua sai

--- src/main/java/com/tas/controller/HomeController.java
+++ src/main/java/com/tas/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String homePage() {
-        return "test";
+        return "home";
     }
 
 }
